67fa93b19cfad94e1385f42078a876e4eb15ed1a
serg081270 <sergeyrykov@Sergeys-MacBook-Air.local>
2022-08-15 10:23:05 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Не !!Редактируй <code>src/App.js</code>, сохраняй, оно будет reboot.
         </p>
         <a
           className="App-link"
