5952b77c70afd7e3cb2dec3e23c5e18d8c91533e
serg081270 <sergeyrykov@Sergeys-MacBook-Air.local>
2022-08-16 15:17:15 +0200

    just text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Не !!Редактируй <code>src/App.js</code>, сохраняй, оно будет reboot.
+          Еще раз поменяем <code>src/App.js</code>, сохраняй, оно будет reboot.
         </p>
         <a
           className="App-link"
